3d428d98b08492e2befa03569d790bef2944206d
Deeplaksh Yadav <158273923+DeeplakshY@users.noreply.github.com>
2025-12-25 05:11:16 +0530

    Day 02: variables and basic data types

---
+++ basics/day_02_variables_and_types.py
@@ -0,0 +1,11 @@
+# Day 02: Variables and basic data types
+
+age = 21              # int
+height = 5.9          # float
+name = "Deeplaksh"    # string
+is_student = True     # boolean
+
+print("Age:", age)
+print("Height:", height)
+print("Name:", name)
+print("Is student:", is_student)
