13c7e2459a1c3d5e29cad6ee3fc8f2db77dcfcaf
Deeplaksh Yadav <158273923+DeeplakshY@users.noreply.github.com>
2025-12-26 09:40:56 +0530

    Day 03: type casting and user input

---
+++ basics/day_03_type_casting_and_input.py
@@ -0,0 +1,9 @@
+# Day 03: Type casting and user input
+
+age = input("Enter your age: ")
+age = int(age)   # convert string to int
+
+height = float(input("Enter your height: "))
+
+print("Age after 1 year:", age + 1)
+print("Height:", height)
