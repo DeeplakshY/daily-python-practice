26070dc5731ea3e8e7b1ed58a2d09b057224b202
Deeplaksh Yadav <158273923+DeeplakshY@users.noreply.github.com>
2025-12-27 08:41:57 +0530

    Day 04: strings indexing and slicing

---
+++ basics/day_04_strings_indexing_slicing.py
@@ -0,0 +1,9 @@
+# Day 04: Strings, indexing and slicing
+
+text = "PythonProgramming"
+
+print(text)          # full string
+print(text[0])       # first character
+print(text[-1])      # last character
+print(text[0:6])     # slicing
+print(text[6:])      # from index 6 to end
