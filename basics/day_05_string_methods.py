62183b955c2ed12e811ea6e763b3fc2eddfdbc06
Deeplaksh Yadav <158273923+DeeplakshY@users.noreply.github.com>
2025-12-29 08:48:34 +0530

    Day 05: common string methods

---
+++ basics/day_05_string_methods.py
@@ -0,0 +1,9 @@
+# Day 05: Common string methods
+
+text = "  Python is Awesome  "
+
+print(text.upper())        # convert to uppercase
+print(text.lower())        # convert to lowercase
+print(text.strip())        # remove extra spaces
+print(text.replace("Awesome", "Powerful"))
+print(text.split())        # split into list
